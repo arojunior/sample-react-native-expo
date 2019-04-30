dbbef76bda87d76c0d1d57021258d5ee85010cca
Junior Oliveira <arojunior@gmail.com>
2019-04-29 21:09:49 -0300

    #3 printing the results of the data fetching

--- src/components/CategoriesComponent.js
+++ src/components/CategoriesComponent.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
-const CategoriesComponent = () => <View />;
+const CategoriesComponent = ({ categories }) => (
+  <View>{console.log(`categories`, categories)}</View>
+);
+
+CategoriesComponent.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+};
 
 export default CategoriesComponent;
--- src/components/TransactionsComponent.js
+++ src/components/TransactionsComponent.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
-const TransactionsComponent = () => <View />;
+const TransactionsComponent = ({ transactions }) => (
+  <View>{console.log(`transactions`, transactions)}</View>
+);
+
+TransactionsComponent.propTypes = {
+  transactions: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+};
 
 export default TransactionsComponent;
