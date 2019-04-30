4f4934ed384dd8363910feaf7aa8cae3444743f6
Junior Oliveira <arojunior@gmail.com>
2019-04-29 21:32:30 -0300

    move fetch methods from container to a service

--- src/components/TransactionsContainer.js
+++ src/components/TransactionsContainer.js
@@ -1,22 +1,13 @@
 import { compose, withState, lifecycle } from 'recompose';
-import Client from '../lib/index';
-import logger from '../services/logger';
 import TransationsComponent from './TransactionsComponent';
+import { getTransactions } from '../services/transactionsService';
 
 export default compose(
   withState(`transactions`, `setTransactions`, []),
   lifecycle({
     componentDidMount() {
       const { setTransactions } = this.props;
-      const mock = new Client();
-      mock
-        .fetchTransactions()
-        .then(data => {
-          setTransactions(data);
-        })
-        .catch(err => {
-          logger.error(err);
-        });
+      getTransactions(setTransactions);
     }
   })
 )(TransationsComponent);
